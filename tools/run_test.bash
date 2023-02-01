9428682283404f9c9e143a0e761b33e0ccecc934
Yannick SEBASTIA <96914693+ysebastia@users.noreply.github.com>
2023-02-01 19:49:39 +0100

    Revert "Check exit code directly with e.g. 'if mycmd;', not indirectly with $?."
    This reverts commit 33242783c31b7398d08b4a1f7687c08dc6bbb8ea.

--- tools/run_test.bash
+++ tools/run_test.bash
@@ -2,7 +2,8 @@
 for roledir in roles/*/molecule; do
   echo "Test ${roledir}"
     pushd "$(dirname "${roledir}")" || exit 2
-    if [ ! "$(python3 -m molecule test)" -eq 0 ]; then
+    molecule test
+    if [ ! "${?}" -eq 0 ]; then
       molecule destroy
       echo "Erreur ${roledir}"
       exit 1
