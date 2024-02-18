41bea2e833b42fcbb63a4947c5ea6a4c761f94f0
Yannick SEBASTIA <96914693+ysebastia@users.noreply.github.com>
2024-02-18 16:21:20 +0100

    chore: update run_doc script

--- tools/run_doc.bash
+++ tools/run_doc.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+pip install ansible-doctor==4.0.1
 for collection in collections/ansible_collections/*/*/roles/; do
     echo "! ${collection}"
     pushd "$(dirname "${collection}")" || exit 2
