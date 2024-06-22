123e0ac1384b5254dadeeb91d4ce7d072c43dc34
Yannick SEBASTIA <96914693+ysebastia@users.noreply.github.com>
2024-06-22 08:40:29 +0200

    chore: update ansible-doctor to 6.0.0

--- tools/run_doc.bash
+++ tools/run_doc.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-pip install ansible-doctor==4.1.1
+pip install ansible-doctor==6.0.0
 for collection in collections/ansible_collections/*/*/roles/; do
     echo "! ${collection}"
     pushd "$(dirname "${collection}")" || exit 2
