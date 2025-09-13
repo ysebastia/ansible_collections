844b0121adf28770d4825c6af29c1d53ba0eebd2
Yannick SEBASTIA <96914693+ysebastia@users.noreply.github.com>
2025-09-13 08:27:18 +0200

    chore: update ansible-doctor to 7.2.0

--- tools/run_doc.bash
+++ tools/run_doc.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-pip install ansible-doctor==7.0.9
+pip install ansible-doctor==7.2.0
 for collection in collections/ansible_collections/*/*/roles/; do
     echo "! ${collection}"
     pushd "$(dirname "${collection}")" || exit 2
