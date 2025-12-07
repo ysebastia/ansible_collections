c63af9a9b862f89b850356c1bacfbc09c678993e
Yannick SEBASTIA <96914693+ysebastia@users.noreply.github.com>
2025-12-07 12:00:01 +0100

    chore: update ansible-doctor to 8.1.0

--- tools/run_doc.bash
+++ tools/run_doc.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-pip install ansible-doctor==7.2.1
+pip install ansible-doctor==8.1.0
 for collection in collections/ansible_collections/*/*/roles/; do
     echo "! ${collection}"
     pushd "$(dirname "${collection}")" || exit 2
