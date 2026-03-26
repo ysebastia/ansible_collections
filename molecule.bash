942b4d3a843b6813f8f4c489ae9c0490ddc88e4c
Yannick SEBASTIA <96914693+ysebastia@users.noreply.github.com>
2026-03-26 07:35:00 +0100

    chore: update ansible to 2.20.4

--- molecule.bash
+++ molecule.bash
@@ -6,7 +6,7 @@ export DEFAULT_LOCAL_TMP=/tmp/jenkins/
 export PIPX_DEFAULT_PYTHON=/usr/bin/python3.12
 export PATH=$HOME/.local/bin:$PATH
 env
-pipx install ansible-core==2.20.1
+pipx install ansible-core==2.20.4
 pipx install ansible-lint==26.3.0
 pipx install molecule==25.12.0 
 pipx inject molecule molecule-plugins[podman]==25.8.12
